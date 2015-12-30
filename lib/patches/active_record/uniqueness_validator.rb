4528a2a2ac1d14b3f10515be1b57be53b08a6bcc
Torsten Wetzel <torsten@orderlift.com>
2015-12-30 12:21:31 +0100

    change alias_method_chain
    .. not compatible with rails 5.x

--- lib/patches/active_record/uniqueness_validator.rb
+++ lib/patches/active_record/uniqueness_validator.rb
@@ -1,7 +1,50 @@
-require 'active_record/validations/uniqueness.rb'
+# => require 'active_record/validations/uniqueness.rb'
+# => 
+# => ActiveRecord::Validations::UniquenessValidator.class_eval do
+# =>   def validate_each_with_translations(record, attribute, value)
+# =>     klass = record.class
+# =>     if klass.translates? && klass.translated?(attribute)
+# =>       finder_class = klass.translation_class
+# =>       table = finder_class.arel_table
+# => 
+# =>       relation = build_relation(finder_class, table, attribute, value).and(table[:locale].eq(Globalize.locale))
+# =>       relation = relation.and(table[klass.reflect_on_association(:translations).foreign_key].not_eq(record.send(:id))) if record.persisted?
+# => 
+# =>       translated_scopes = Array(options[:scope]) & klass.translated_attribute_names
+# =>       untranslated_scopes = Array(options[:scope]) - translated_scopes
+# => 
+# =>       untranslated_scopes.each do |scope_item|
+# =>         scope_value = record.send(scope_item)
+# =>         reflection = klass.reflect_on_association(scope_item)
+# =>         if reflection
+# =>           scope_value = record.send(reflection.foreign_key)
+# =>           scope_item = reflection.foreign_key
+# =>         end
+# =>         relation = relation.and(find_finder_class_for(record).arel_table[scope_item].eq(scope_value))
+# =>       end
+# => 
+# =>       translated_scopes.each do |scope_item|
+# =>         scope_value = record.send(scope_item)
+# =>         relation = relation.and(table[scope_item].eq(scope_value))
+# =>       end
+# => 
+# =>       if klass.unscoped.with_translations.where(relation).exists?
+# =>         record.errors.add(attribute, :taken, options.except(:case_sensitive, :scope).merge(:value => value))
+# =>       end
+# =>     else
+# =>       validate_each_without_translations(record, attribute, value)
+# =>     end
+# =>   end
+# =>   alias_method_chain :validate_each, :translations
+# => end
 
-ActiveRecord::Validations::UniquenessValidator.class_eval do
-  def validate_each_with_translations(record, attribute, value)
+
+
+require 'active_support/core_ext/module/aliasing'
+
+module UniquenessValidationWithTranslations #:nodoc:
+  
+  def validate_each(record, attribute, value)
     klass = record.class
     if klass.translates? && klass.translated?(attribute)
       finder_class = klass.translation_class
@@ -32,8 +75,10 @@ ActiveRecord::Validations::UniquenessValidator.class_eval do
         record.errors.add(attribute, :taken, options.except(:case_sensitive, :scope).merge(:value => value))
       end
     else
-      validate_each_without_translations(record, attribute, value)
+      super(record, attribute, value)
     end
   end
-  alias_method_chain :validate_each, :translations
+  
 end
+
+ActiveRecord::Validations::UniquenessValidator.send(:prepend, UniquenessValidationWithTranslations)
